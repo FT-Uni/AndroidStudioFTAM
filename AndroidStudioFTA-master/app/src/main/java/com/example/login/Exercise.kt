713544a46ee3758933a31eb08ad2dbba0f837846
FT-Uni <127231051+FT-Uni@users.noreply.github.com>
2024-01-09 23:55:44 +0000

    Delete AndroidStudioFTA-master directory

--- AndroidStudioFTA-master/app/src/main/java/com/example/login/Exercise.kt
+++
@@ -1,3 +0,0 @@
-package com.example.login
-
-data class Exercise(val id: Int, val name: String, val targetMuscle: String, var sets: Int = 0, var reps: Int = 0, var isExpanded: Boolean = false)
--- AndroidStudioFTA-master/app/src/main/java/com/example/login/PastExercises.kt
+++
@@ -1,3 +0,0 @@
-package com.example.login
-
-data class PastExercises(val name: String, val muscleGroup: String, val date: String, val day: String, val weekNumber: Int)
