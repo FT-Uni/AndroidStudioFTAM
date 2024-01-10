0f45dcd36958c7070fc8754a3042391099f97c03
Farhaan <Farhaan_tariq@hotmail.com>
2024-01-10 00:02:33 +0000

    fix imports

--- app/src/main/java/com/example/login/ExerciseListActivity.kt
+++ app/src/main/java/com/example/login/ExerciseListActivity.kt
@@ -1,6 +1,8 @@
 package com.example.login
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
